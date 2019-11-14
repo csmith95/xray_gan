ed3fa92a6b4adc23f28cbd094189941226a4c0ae
Raging Panda <ragingpanda@Ragings-MacBook-Pro.local>
2019-11-14 14:11:02 -0800

    fixed normalization values for grayscale

--- classifier/my_dataloaders.py
+++ classifier/my_dataloaders.py
@@ -63,7 +63,7 @@ with open('../data/labels.data', 'rb') as filehandle:
 data_transforms = transforms.Compose([
 	transforms.Resize(hyperparams.input_size),
     transforms.ToTensor(),
-    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5]) # normalize grayscale RGB to range [-1, 1]
 ])
 torchvision.set_image_backend('accimage')
 data_dir = '../data/images/'	
