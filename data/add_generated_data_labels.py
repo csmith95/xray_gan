bbfdf8987d743bed9dbf0cce05a2a04e91063a71
Raging Panda <ragingpanda@Ragings-MacBook-Pro.local>
2019-12-05 21:26:41 -0800

    same as previous comm

--- data/add_generated_data_labels.py
+++ data/add_generated_data_labels.py
@@ -2,30 +2,27 @@ import pickle
 from pathlib import Path
 
 # fetch current labels
-labels = None
-with open('./labels.data', 'rb') as f:
-	labels = pickle.load(f)
-	assert labels != None
-
-
-print(labels)
+# labels = None
+# with open('./labels.data', 'rb') as f:
+# 	labels = pickle.load(f)
+# 	assert labels != None
 
 # add generated data labels
-#filenames = []
-#for subdr, label in zip(['healthy', 'unhealthy'], [1, 0]):
-#	paths = Path('./generated_images/{}/'.format(subdr)).glob('*.png')
-#	for path in paths:
-#		filename = str(path).split('/')[-1]
-#		filenames.append(filename)
-#		labels[filename] = label
+filenames = []
+for subdr, label in zip(['healthy', 'unhealthy'], [1, 0]):
+	paths = Path('./generated_images/{}/'.format(subdr)).glob('*.png')
+	for path in paths:
+		filename = str(path).split('/')[-1]
+		filenames.append(filename)
+		# labels[filename] = label
 
-#print(filenames)
+print(filenames)
 
-#with open('./generated_image_ids.data', 'wb') as f:
- #   pickle.dump(filenames, f)
+with open('./generated_image_ids.data', 'wb') as f:
+   pickle.dump(filenames, f)
 
 # overwrite labels with legit+generated labels
-#with open('./labels.data', 'wb') as f:
-    #pickle.dump(labels, f)
+# with open('./labels.data', 'wb') as f:
+#     pickle.dump(labels, f)
 
 #print(labels)
